ee319bc585d14c6a5246f0e2eb9cdb52062001ab
Mohammad Alnabale <alnabale@insalat.com>
2022-10-20 09:57:39 +0300

    D

--- Array/2-Medium/Q5-SpiralTraverse.js
+++ Array/2-Medium/Q5-SpiralTraverse.js
@@ -37,5 +37,41 @@ O(n) time | O(n) space - where n is the total number of elements in the array
   */
 
 function spiralTraverse(array) {
-	// Write your code here.
+	const result = [];
+	let startRow = 0,
+		endRow = array.length - 1;
+	let startCol = 0,
+		endCol = array[0].length - 1;
+
+	while (startRow <= endRow && startCol <= endCol) {
+		for (let col = startCol; col <= endCol; col++) {
+			result.push(array[startRow][col]);
+		}
+
+		for (let row = startRow + 1; row <= endRow; row++) {
+			result.push(array[row][endCol]);
+		}
+
+		for (let col = endCol - 1; col >= startRow; col--) {
+			// the break here because if we have a single line to not dedicate the value like below array example
+			/*
+				[1, 2, 3, 4],
+				[12, 13, 14, 5],
+				[11, 16, 15, 6],
+			*/
+			if (startRow === endRow) break;
+			result.push(array[endRow][col]);
+		}
+
+		for (let row = endRow - 1; row > startRow; row--) {
+			if (startCol === endCol) break;
+			result.push(array[row][startCol]);
+		}
+
+		startRow++;
+		endRow--;
+		startCol++;
+		endCol--;
+	}
+	return result;
 }
