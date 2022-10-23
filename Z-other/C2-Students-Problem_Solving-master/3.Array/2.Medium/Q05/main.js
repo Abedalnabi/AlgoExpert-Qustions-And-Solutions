8e618fe1aa5a1e2da736b6cc2a664a6a234c5c1c
Mohammad Alnabale <alnabale@insalat.com>
2022-10-23 08:43:42 +0300

    E

--- Z-other/C2-Students-Problem_Solving-master/3.Array/2.Medium/Q05/main.js
+++ Z-other/C2-Students-Problem_Solving-master/3.Array/2.Medium/Q05/main.js
@@ -7,9 +7,40 @@ Write a function accepts an array of integers and returns the Maximum product po
 */
 
 const MaximumProductOfThree = (arr) => {
-  // YOUR CODE HERE
-}
+	// O(n log n)
+	sortedArr = arr.sort((a, b) => a - b);
+	let len = arr.length;
+	let maxProduct = sortedArr[len - 1] * sortedArr[len - 2] * sortedArr[len - 3];
+	return maxProduct;
+};
 
+const MaximumProductOfThree_2 = (arr) => {
+	let highest = Math.max(arr[0], arr[1]);
+	let lowest = Math.min(arr[0], arr[1]);
+	let highestProductOf2 = arr[0] * arr[1];
+	let lowestProductOf2 = arr[0] * arr[1];
+	let highestProductOf3 = arr[0] * arr[1] * arr[2];
+
+	for (let i = 2; i < arr.length; i++) {
+		let current = arr[i];
+
+		highestProductOf3 = Math.max(
+			highestProductOf3,
+			current * highestProductOf2,
+			current * lowestProductOf2
+		);
+
+		highestProductOf2 = Math.max(highestProductOf2, current * highest, current * lowest);
+
+		lowestProductOf2 = Math.min(lowestProductOf2, current * highest, current * lowest);
+
+		highest = Math.max(highest, current);
+
+		lowest = Math.min(lowest, current);
+	}
+
+	return highestProductOf3;
+};
 /*
 Examples:
 MaximumProductOfThree([3, 1, 3, 7]) // => 63
--- Z-other/C2-Students-Problem_Solving-master/3.Array/3.Hard/Q01/main.js
+++ Z-other/C2-Students-Problem_Solving-master/3.Array/3.Hard/Q01/main.js
@@ -7,8 +7,16 @@ Write a function returns the length of the longest sequence of 1s in an array of
 */
 
 const longestSequence = (arr) => {
-  // YOUR CODE HERE
-}
+	// let longest = 0;
+	// let currentLongest = 0;
+	// for (const value of arr) {
+	// 	if (currentLongest > longest) longest = currentLongest;
+	// 	if (value === 0) currentLongest = 0;
+
+	// 	if (value === 1) currentLongest++;
+	// }
+	// return longest;
+};
 
 /*
 Examples:
@@ -16,7 +24,7 @@ longestSequence([0, 1, 0, 0, 1, 1, 1, 1,1, 0, 1, 1]) // => 5
 longestSequence([1, 1, 0, 1, 1, 0, 1, 0, 1, 1,1]) // => 3
 longestSequence([1, 1, 0, 1, 1, 1,1, 0, 1, 0, 1, 1]) // => 4
 longestSequence([1, 1, 0, 1, 1, 1,1, 1, 0, 1, 0, 1, 1]) // => 5
-longestSequence([1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1,1]) // => 6
+longestSequence([1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1]) // => 6
 longestSequence([1, 0, 0, 1]) // => 1
 longestSequence([0, 0, 0, 0]) // => 'There is no one sequenced'
 */
