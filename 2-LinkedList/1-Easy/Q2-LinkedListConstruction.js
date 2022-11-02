a7002776d6893e37305e260766417f0f7f2522ff
Mohammad Alnabale <alnabale@insalat.com>
2022-11-02 17:01:38 +0300

    F

--- 2-LinkedList/1-Easy/Q2-LinkedListConstruction.js
+++ 2-LinkedList/1-Easy/Q2-LinkedListConstruction.js
@@ -2,13 +2,18 @@
 
 Linked List Construction
 
-	Write a DoublyLinkedList class that has a head and a tail , both of which point to either a linked list Node or None / null . The class should support:
+	Write a DoublyLinkedList class that has a head and a tail , both of which point to either a linked list Node or None / null . 
+	The class should support:
 	Setting the head and tail of the linked list.
 	Inserting nodes before and after other nodes as well as at given positions (the position of the head node is 1 ).
 	Removing given nodes and removing nodes with given values.
 	Searching for nodes with given values.
-	Note that the setHead , setTail , insertBefore , insertAfter , insertAtPosition , and remove methods all take in actual Node s as input parameters—not integers (except for insertAtPosition , which also takes in an integer representing the position); this means that you don't need to create any new
-	Node s in these methods. The input nodes can be either stand-alone nodes or nodes that are already in the linked list. If they're nodes that are already in the linked list, the methods will effectively be moving the nodes within the linked list. You won't be told if the input nodes are already in the linked list, so your code will have to
+	Note that the setHead , setTail , insertBefore , insertAfter , insertAtPosition , and remove methods all take in actual Node s as input 
+	parameters—not integers (except for insertAtPosition , which also takes in an integer representing the position); 
+	this means that you don't need to create any new
+	Node s in these methods. The input nodes can be either stand-alone nodes or nodes that are already in the linked list. 
+	If they're nodes that are already in the linked list, the methods will effectively be moving the nodes within the linked list. 
+	You won't be told if the input nodes are already in the linked list, so your code will have to
 	defensively handle this scenario.
 	If you're doing this problem in an untyped language like Python or JavaScript, you may want to look at the various function signatures in a typed language like Java or TypeScript to get a better idea of what each input parameter is.
 	Each Node has an integer value as well as a prev node and a next node, both of which can point to either another node or None / null .
@@ -63,52 +68,6 @@ class Node {
 		this.next = null;
 	}
 }
-
-// Feel free to add new properties and methods to the class.
-class DoublyLinkedList {
-	constructor() {
-		this.head = null;
-		this.tail = null;
-	}
-
-	setHead(node) {
-		this.head = node
-	}
-
-	setTail(node) {
-		// Write your code here.
-	}
-
-	insertBefore(node, nodeToInsert) {
-		// serch node
-		// when find curr.pre = nodeToInsert
-		// nodeToInsert.next = curr
-		// nodeToInsert.pre = curr.pre.pre
-		// nodeToInsert.pre = curr.pre.pre
-
-	}
-
-	insertAfter(node, nodeToInsert) {
-		// Write your code here.
-	}
-
-	insertAtPosition(position, nodeToInsert) {
-		// Write your code here.
-	}
-
-	removeNodesWithValue(value) {
-		// Write your code here.
-	}
-
-	remove(node) {
-		// Write your code here.
-	}
-
-	containsNodeWithValue(value) {
-		// Write your code here.
-	}
-}
-
 
 class DoublyLinkedList {
 	constructor() {
@@ -201,3 +160,21 @@ class DoublyLinkedList {
 		node.next = null;
 	}
 }
+
+let a = new Node(1);
+let b = new Node(2);
+let c = new Node(3);
+let d = new Node(4);
+
+console.log('a', a);
+console.log('d', d);
+
+let list = new DoublyLinkedList();
+list.setHead(a);
+list.insertAfter(a, b);
+list.insertAfter(b, c);
+list.insertAfter(b, d);
+
+list.remove(d);
+
+console.log('list', list);
