1fce9be90d09ff32d58447eb102080279abacfea
Mohammad Alnabale <alnabale@insalat.com>
2022-11-08 11:19:47 +0300

    F

--- 2-LinkedList/2-Medium/Q3-SumofLinkedLists.js
+++ 2-LinkedList/2-Medium/Q3-SumofLinkedLists.js
@@ -21,17 +21,32 @@ Sample Output
 // linkedListTwo represents 549
 // 1742 + 549 = 2291
 
+	1742 + 
+     549
+  = 2291
+
 Hints
 Hint 1
-If you can determine the integers that each individual Linked List represents, then all you need to do is add these integers and create a new Linked List that represents the summed value.
+If you can determine the integers that each individual Linked List represents, 
+then all you need to do is add these integers and create a new Linked List that represents the summed value.
+
 Hint 2
-If you go with the approach mentioned in Hint #1, you'll need to break down the sum of the two Linked Lists' numbers into its individual digits. Once you know these digits, you can create a new Linked List using them. This approach is fine, but you can solve this problem more elegantly, with a single iteration through the
+If you go with the approach mentioned in Hint #1, you'll need to break down the sum of the two Linked Lists' 
+numbers into its individual digits. Once you know these digits, you can create a new Linked List using them. 
+This approach is fine, but you can solve this problem more elegantly, with a single iteration through the
 Linked Lists.
+
 Hint 3
-Is it necessary to know the entire numbers represented by both Linked Lists in order to calculate their sum? Think back to your elementary-school math class; how did you add two numbers together?
+Is it necessary to know the entire numbers represented by both Linked Lists in order to calculate their sum? Think back to your 
+elementary-school math class; how did you add two numbers together?
+
 Hint 4
-Since each Linked List's digits are ordered from least significant digit to most significant digit, you can simply loop through both Linked Lists, consider the digits with the same significance, and add these digits together while keeping track of any carry that comes out of the addition. At each iteration, when you add the two
-Linked List digits, also add the carry from the previous iteration. Create a new Linked List node that stores the calculated value, and add that to your new Linked List. Keep iterating until you reach the end of both Linked Lists and have no remaining carry.
+Since each Linked List's digits are ordered from least significant digit to most significant digit, you can simply loop through both Linked Lists, 
+consider the digits with the same significance, and add these digits together while keeping track of any carry that comes out of the addition.
+At each iteration, when you add the two Linked List digits, also add the carry from the previous iteration. 
+Create a new Linked List node that stores the calculated value, 
+and add that to your new Linked List. Keep iterating until you reach the end of both Linked Lists and have no remaining carry.
+
 Optimal Space & Time Complexity
 O(max(n, m)) time | O(max(n, m)) space - where n is the length of the first Linked List and m is the length of the second Linked List
 
@@ -47,6 +62,27 @@ class LinkedList {
 	}
 }
 
+// O(max(n, m)) time | O(max(n, m)) space - where n is the length of the
+// first Linked List and m is the length of the second Linked List
 function sumOfLinkedLists(linkedListOne, linkedListTwo) {
-	return linkedListOne;
+	// This variable will store a dummy node whose .next
+	// attribute will point to the head of our new LL.
+	const newLinkedListHeadPointer = new LinkedList(0);
+	let currentNode = newLinkedListHeadPointer;
+	let carry = 0;
+	let nodeOne = linkedListOne;
+	let nodeTwo = linkedListTwo;
+	while (nodeOne !== null || nodeTwo !== null || carry !== 0) {
+		const valueOne = nodeOne !== null ? nodeOne.value : 0;
+		const valueTwo = nodeTwo !== null ? nodeTwo.value : 0;
+		const sumOfValues = valueOne + valueTwo + carry;
+		const newValue = sumOfValues % 10;
+		const newNode = new LinkedList(newValue);
+		currentNode.next = newNode;
+		currentNode = newNode;
+		carry = Math.floor(sumOfValues / 10);
+		nodeOne = nodeOne !== null ? nodeOne.next : null;
+		nodeTwo = nodeTwo !== null ? nodeTwo.next : null;
+	}
+	return newLinkedListHeadPointer.next;
 }
