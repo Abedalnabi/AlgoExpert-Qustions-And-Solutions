b36aadd5165965ccf01a5cc9bb4e6e84197470d9
Mohammad Alnabale <alnabale@insalat.com>
2022-11-08 11:32:21 +0300

    F

--- 2-LinkedList/2-Medium/Q3-SumofLinkedLists.js
+++ 2-LinkedList/2-Medium/Q3-SumofLinkedLists.js
@@ -72,17 +72,17 @@ function sumOfLinkedLists(linkedListOne, linkedListTwo) {
 	let carry = 0;
 	let nodeOne = linkedListOne;
 	let nodeTwo = linkedListTwo;
-	while (nodeOne !== null || nodeTwo !== null || carry !== 0) {
-		const valueOne = nodeOne !== null ? nodeOne.value : 0;
-		const valueTwo = nodeTwo !== null ? nodeTwo.value : 0;
+	while (!nodeOne || nodeTwo || carry) {
+		const valueOne = nodeOne ? nodeOne.value : 0;
+		const valueTwo = nodeTwo ? nodeTwo.value : 0;
 		const sumOfValues = valueOne + valueTwo + carry;
 		const newValue = sumOfValues % 10;
 		const newNode = new LinkedList(newValue);
 		currentNode.next = newNode;
 		currentNode = newNode;
 		carry = Math.floor(sumOfValues / 10);
-		nodeOne = nodeOne !== null ? nodeOne.next : null;
-		nodeTwo = nodeTwo !== null ? nodeTwo.next : null;
+		nodeOne = nodeOne ? nodeOne.next : null;
+		nodeTwo = nodeTwo ? nodeTwo.next : null;
 	}
 	return newLinkedListHeadPointer.next;
 }
